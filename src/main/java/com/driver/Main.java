da5af7e66c99c9a114138dc20406b18c921e6747
Your Name <Vijay>
2023-01-16 14:18:30 +0530

    Solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String [] args){
+      B b=new B();
+      b.meth();
+  }
+}
+class A{
+    String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+
 }
